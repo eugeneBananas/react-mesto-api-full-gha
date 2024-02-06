8367d73ebe6203f03eec89c7c2d27a3ca620978d
eugeneBananas <rypolov.e@mail.ru>
2024-02-06 18:03:26 +0300

    att 1.4

--- backend/app.js
+++ backend/app.js
@@ -11,8 +11,7 @@ const auth = require('./middlewares/auth');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const NotFoundError = require('./errors/not-found-error');
 
-const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } =
-  process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const app = express();
 
